a342e2f87da6e4b04eb1405124d86ec193a37023
nasserheydari <nah315@lehigh.edu>
2020-08-17 19:46:45 -0400

    added files

---
+++ demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def printnum(i):
+    print(i)
+
+
+# In[5]:
+
+
+for i in range(5):
+    printnum(i)
+
---
+++ demo1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+print('Nasser Heydari')
+print(np.array(1))
+print("This is a change")
